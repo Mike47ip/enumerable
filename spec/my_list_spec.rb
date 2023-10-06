f9e0fe10aa8c15558dacf9df2b377e4c72f812a0
Mike Dark <agyemankojo47@gmail.com>
2023-10-06 13:09:53 +0000

    Create test

---
+++ spec/my_list_spec.rb
@@ -0,0 +1,24 @@
+# my_list_test.rb
+require 'minitest/autorun'
+require_relative '../my_list'
+
+class MyListTest < Minitest::Test
+  def setup
+    @list = MyList.new(1, 2, 3, 4)
+  end
+
+  def test_all_method
+    assert_equal true, @list.all? { |e| e < 5 }
+    assert_equal false, @list.all? { |e| e > 5 }
+  end
+
+  def test_any_method
+    assert_equal true, @list.any? { |e| e == 2 }
+    assert_equal false, @list.any? { |e| e == 5 }
+  end
+
+  def test_filter_method
+    assert_equal [2, 4], @list.filter { |e| e.even? }
+    assert_equal [1, 3], @list.filter { |e| e.odd? }
+  end
+end
