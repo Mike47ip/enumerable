561431eea6cab785052b883801c8e4047fd829e3
Mike Dark <agyemankojo47@gmail.com>
2023-10-06 13:09:36 +0000

    Create list and enumerable

---
+++ my_enumerable.rb
@@ -0,0 +1,18 @@
+# my_enumerable.rb
+module MyEnumerable
+  def all?
+    each { |item| return false unless yield item }
+    true
+  end
+
+  def any?
+    each { |item| return true if yield item }
+    false
+  end
+
+  def filter
+    result = []
+    each { |item| result << item if yield item }
+    result
+  end
+end
---
+++ my_list.rb
@@ -0,0 +1,28 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*items)
+    @list = items
+  end
+
+  def each
+    @list.each { |item| yield item }
+  end
+end
+
+
+# Example usage
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? { |e| e < 5 } # Should output: true
+puts list.all? { |e| e > 5 } # Should output: false
+
+# Test #any?
+puts list.any? { |e| e == 2 } # Should output: true
+puts list.any? { |e| e == 5 } # Should output: false
+
+# Test #filter
+puts list.filter { |e| e.even? } # Should output: [2, 4]
